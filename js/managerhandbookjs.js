1f7409beb0b5677af716257c70e453cac459ed91
Marvel Okafor <oka17002@byui.edu>
2020-07-30 11:27:49 -0600

    update

---
+++ js/managerhandbookjs.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByClassName("container"));
+
+console.log(document.querySelectorAll('container'));
+
+let containers = console.log(document.getElementById("containers").classList.toggle("game"));
