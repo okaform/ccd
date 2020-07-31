601e9384c7b1fe569d6d6ce07c6bb52bb5fc5cd4
Marvel Okafor <oka17002@byui.edu>
2020-07-30 20:04:45 -0600

    update

--- js/managerhandbookjs.js
+++ js/managerhandbookjs.js
@@ -3,3 +3,17 @@
 // console.log(document.querySelectorAll('container'));
 
 // let containers = console.log(document.getElementById("containers").classList.toggle("game"));
+
+//This function is for the nav bar
+
+function navOpen() {
+    let y = document.getElementById("navContainer").classList.toggle("hide");
+    document.getElementById("navContainer").style.width = "84%";
+    
+   // let c = document.getElementById("changeSymbol").innerHTML = "&#935;";
+
+   if (y == true) {
+    //c = document.getElementById("changeSymbol").innerHTML = "&#9776;";
+    document.getElementById("navContainer").style.width = "0";
+   }
+}
